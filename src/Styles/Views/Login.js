9af5bebd2f77c51a7a05fd78e28d849cfa907cae
Mahdi <mahdi.unic1@gmail.com>
2023-12-01 10:41:26 +0100

    Login form is done disgining

--- src/Styles/Views/Login.js
+++ src/Styles/Views/Login.js
@@ -7,12 +7,66 @@ export const LoginStyle = (StyleSheet.createstyle = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
-    paddingTop: 300,
+    justifyContent:'center',
   },
+
   logo: {
     width:"10%",
-    height:"5%"
-  }
+    height:"5%",
+  },
+
+  mainTitle: {marginVertical: 30, fontSize: 22},
+
+  form: {width:  '70%'},
+
+  input: {
+    width:  '100%',
+    borderWidth:  1,
+    height:  '44',
+    borderColor:  '#e5e5e5',
+    marginVertical:  10,
+    borderRadius:  9,
+  },
+
+  passwordLabelsContainer: {
+    display:  'flex',
+    flexDirection:  'row',
+    justifyContent:  'space-between',
+  },
+
+  passwordLabel: {},
+
+  forgetPassword: {color:  '#4F46E5'},
+
+  signinButton: {
+    backgroundColor:  '#4F46E5',
+    height:  44,
+    borderRadius:  9,
+    display:  'flex',
+    flexDirection:  'column',
+    justifyContent:  'center',
+    alignItems:  'center',
+  },
+
+  signinText: {color:  'white'},
+
+  trialContainer: {
+    display:  'flex',
+    flexDirection:  'row',
+    justifyContent:  'center',
+    marginTop:  30,
+  },
   
+  trialSpan: {
+    color: '#4F46E5',
+    marginLeft: 5,
+  },
+  errorMessage: {
+    fontSize: 12,
+    marginBottom: 20,
+    color: 'red',
+  },
+
+
 
 }))
--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,27 +1,64 @@
 import { View, Text, Image,TextInput, TouchableOpacity } from "react-native"
 import { LoginStyle } from "../Styles/Views/Login"
 import Logo from "../medias/mark.png"
+import { useState } from "react"
+import { checkEmail, checkPassword } from "../Utils/Regex"
 
 export default Login = ()=>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    function handleLogin(){
+
+        if(checkEmail(email) === false){
+            setEmailError("Incorrect email format!")
+        }
+        
+        if(checkPassword(password) === false){
+            setPasswordError("Incorrect password format!")
+        }
+    }
+
     return(
         <View style={LoginStyle.mainContainer}>
             <Image source={Logo} style={LoginStyle.logo}/>
-            <Text>Sign in your account</Text>
+            <Text style={LoginStyle.mainTitle}>Sign in your account</Text>
 
-            <View>
+            <View style={LoginStyle.form}>
                 <Text>Email Adress</Text>
-                <TextInput onChange={()=>{}}/> 
+                <TextInput 
+                style={LoginStyle.input} 
+                onChangeText={setEmail}
+                inputMode="email"
+                /> 
+                <Text style={LoginStyle.errorMessage}>
+                    {emailError && emailError}
+                </Text>
 
-                <View>
+                <View style={LoginStyle.passwordLabelsContainer}>
                     <Text>Password</Text>
-                    <Text>Forget Password</Text>
+                    <Text style={LoginStyle.forgetPassword}>Forget Password</Text>
+                </View>
+                <TextInput 
+                style={LoginStyle.input} 
+                onChangeText={setPassword} 
+                secureTextEntry
+                /> 
+                <Text  style={LoginStyle.errorMessage}>
+                    {passwordError && passwordError}
+                </Text>
+
+                <TouchableOpacity style={LoginStyle.signinButton} onPress={handleLogin} >
+                    <Text style={LoginStyle.signinText} >Sign in</Text>
+                </TouchableOpacity>
+
+                <View  style={LoginStyle.trialContainer}>
+                    <Text>Not a member?</Text>
+                    <Text style={LoginStyle.trialSpan}>Start a 14 days trial</Text>
                 </View>
                 
-                <TextInput onChange={()=>{}}/> 
-                <TouchableOpacity>
-                    <Text>Sign in</Text>
-                </TouchableOpacity>
-                <Text>Not a member? try 14 days trial</Text>
             </View>
         </View>
     )
